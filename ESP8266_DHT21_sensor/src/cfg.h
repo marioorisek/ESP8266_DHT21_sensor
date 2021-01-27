be3354ca8506d2b66cf77396625c4bcbb46ad301
marioorisek <bofhik42@seznam.cz>
2021-01-27 19:04:45 +0100

    Inverter display orientation

--- ESP8266_DHT21_sensor/src/cfg.h
+++ ESP8266_DHT21_sensor/src/cfg.h
@@ -3,4 +3,3 @@
 #define SCREEN_DELAY 4000
 
 #define FONT_LARGE u8g2_font_helvB24_te
-#define FONT_SMALL u8g2_font_helvB14_te
--- ESP8266_DHT21_sensor/src/main.cpp
+++ ESP8266_DHT21_sensor/src/main.cpp
@@ -7,7 +7,7 @@
 #include "cfg.h"
 #include "wifi_cfg.h"
 
-U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R2, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
 DHT dht(DHT_SENSOR_PIN, DHT_TYPE);
 
 float temperature = 0.0;
@@ -15,8 +15,8 @@ float humidity = 0;
 
 void drawScreen(byte screenId) {
       u8g2.clearBuffer(); // clear the internal memory
-      delay(100);
       u8g2.setFont(FONT_LARGE);
+
   switch (screenId) {
     case 0:  // splash screen
       Serial.println("Splash screen displayed.");
@@ -51,14 +51,14 @@ void measure() {
   Serial.print(" deg. C | Humidity: ");
   Serial.print(humidity, 0);
   Serial.println(" %");
-  delay(200);
+
 }
 
 void setup() {
   Serial.begin(115200);
   u8g2.begin();
   dht.begin();
-  delay(500);
+  delay(1000);
 
   Serial.println();
   Serial.println("Sensor init..");
